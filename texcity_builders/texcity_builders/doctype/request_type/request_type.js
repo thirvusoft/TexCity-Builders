ba78b5d7296842acad433924157761c430339566
Shankarv19bcr <shankar@thirvusoft.in>
2022-12-21 17:34:41 +0530

    create doctypes

---
+++ texcity_builders/texcity_builders/doctype/request_type/request_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Thirvusoft Private Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Request Type', {
+	// refresh: function(frm) {
+
+	// }
+});
