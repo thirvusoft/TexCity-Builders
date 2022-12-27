a1026b4820284ae016ddc01b5acd389026ad50a3
Shankarv19bcr <shankar@thirvusoft.in>
2022-12-27 15:37:09 +0530

    after install

--- texcity_builders/install.py
+++ texcity_builders/install.py
@@ -7,7 +7,7 @@ def after_install():
 
 def create_roles():
     if(not frappe.db.exists('Role', 'Texcity Admin')):
-        doc = frappe.new_doc('Role', 'Texcity Admin')
+        doc = frappe.new_doc('Role')
         doc.role_name = 'Texcity Admin'
         doc.save()
 
@@ -15,7 +15,7 @@ def create_lead_types():
     lead_types = ['Channel Partner', 'Client', 'Consultant']
     for i in lead_types:
         if(not frappe.db.exists('Lead Type', i)):
-            doc = frappe.new_doc('Lead Type', i)
+            doc = frappe.new_doc('Lead Type')
             doc.lead_type = i
             doc.save()
 
@@ -23,6 +23,6 @@ def create_plot_type():
     plot_types = ['Appartment', 'Land Only', 'Land With Building', 'Ready TO Occupy']
     for i in plot_types:
         if(not frappe.db.exists('Plot Type', i)):
-            doc = frappe.new_doc('Plot Type', i)
+            doc = frappe.new_doc('Plot Type')
             doc.plot_type = i
             doc.save()
