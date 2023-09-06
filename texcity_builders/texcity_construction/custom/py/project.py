e41e7894969f94423aa1e269bfb3d32e59b22237
Nandhinidevi123 <nandhinidevi@thirvusoft.in>
2023-09-06 10:12:22 +0000

    chnages

--- texcity_builders/texcity_construction/custom/py/project.py
+++ texcity_builders/texcity_construction/custom/py/project.py
@@ -5,13 +5,14 @@ def warehouse_creation(doc,event):
         warehouse=frappe.new_doc("Warehouse")
         warehouse.warehouse_name=doc.project_name
         warehouse.is_group=doc.is_group
-        warehouse.parent_warehouse=doc.parent_project
+        warehouse.parent_warehouse=doc.custom_warehouse or ""
         warehouse.company=doc.company
         warehouse.save(ignore_permissions = True)
+        doc.custom_warehouse=warehouse.name
     else:
         warehouse_doc=frappe.get_doc("Warehouse",{"warehouse_name":doc.name})
         warehouse_doc.update({
             'is_group': doc.is_group,
-            'parent_warehouse': doc.parent_project
+            'parent_warehouse': doc.custom_warehouse
         })
         warehouse_doc.save()
