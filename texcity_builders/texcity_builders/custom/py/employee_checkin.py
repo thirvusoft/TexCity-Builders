421369d943152504324d83e7f49ebf48c00b21b3
Shankarv19bcr <shankar@thirvusoft.in>
2023-11-01 09:15:47 +0000

    add shift in checkin

--- texcity_builders/texcity_builders/custom/py/employee_checkin.py
+++ texcity_builders/texcity_builders/custom/py/employee_checkin.py
@@ -1,17 +1,29 @@
 import frappe
 from frappe.utils import now_datetime, nowdate
+from hrms.hr.doctype.shift_assignment.shift_assignment import get_employee_shift
+
+
+def get_default_shift(employee=None):
+    if not employee:
+        return ""
+    shift_details = get_employee_shift(employee)
+    if shift_details:
+        return shift_details["shift_type"]["name"]
+    return ""
 
 @frappe.whitelist()
 def create_checkin(lat=None, long=None):
     emp = frappe.db.get_value("Employee", {"status":"Active", "user_id":frappe.session.user}, "name")
     doc = frappe.new_doc("Employee Checkin")
+    shift_type = get_default_shift(emp)
     doc.update({
         "user":frappe.session.user,
         "employee":emp,
         "log_type":"IN",
         "time":now_datetime(),
         "checkin_latitude":lat,
-        "checkin_longitude":long
+        "checkin_longitude":long,
+        "shift":shift_type
     })
     doc.flags.ignore_mandatory = True
     doc.insert()
@@ -21,13 +33,15 @@ def create_checkin(lat=None, long=None):
 def create_checkout(lat=None, long=None):
     emp = frappe.db.get_value("Employee", {"status":"Active", "user_id":frappe.session.user}, "name")
     doc = frappe.new_doc("Employee Checkin")
+    shift_type = get_default_shift(emp)
     doc.update({
         "user":frappe.session.user,
         "employee":emp,
         "log_type":"OUT",
         "time":now_datetime(),
         "checkout_latitude":lat,
-        "checkout_longitude":long
+        "checkout_longitude":long,
+        "shift":shift_type
     })
     doc.flags.ignore_mandatory = True
     doc.insert()
